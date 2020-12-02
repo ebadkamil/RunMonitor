fbc5b46db1017f11aca43ccf552b9dcde3d8901b
Ebad Kamil <ebad.kamil@xfel.eu>
2020-12-02 12:39:45 +0100

    Remove unnecessary functions from utils

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -4,14 +4,9 @@ Run Visualization software
 Author: Ebad Kamil <ebad.kamil@xfel.eu>
 All rights reserved.
 """
-from datetime import datetime
-from functools import lru_cache
 from glob import iglob
 import os
-from pathlib import Path
 
-from extra_data.validation import FileValidator, RunValidator
-from extra_data import H5File
 
 def find_proposal(proposal, data='raw'):
     """From EXtra-data
@@ -75,62 +70,8 @@ def get_size_format(b, unit="B", factor=1024):
     return b
 
 
-@lru_cache(maxsize=300)
-def get_run_size(directory, creation_date):
-    """Returns the `directory` size in bytes."""
-    total = 0
-    try:
-        # print("[+] Getting the size of", directory)
-        for entry in os.scandir(directory):
-            if entry.is_file():
-                # if it's a file, use stat() function
-                total += entry.stat().st_size
-
-    except NotADirectoryError:
-        # if `directory` isn't a directory, get the file size then
-        total = os.path.getsize(directory)
-    except PermissionError:
-        # if for whatever reason we can't open the folder, return 0
-        return 0
-
-    if os.path.isdir(directory):
-        validator = RunValidator(directory)
-    elif directory.endswith('.h5'):
-        validator = FileValidator(H5File(directory).files[0])
-    else:
-        return 0
-
-    validator.run_checks()
-
-    return total, not validator.problems
-
-
-def get_all_runs_size(path):
-    # iterate over all the directories inside this path
-    try:
-        all_runs = os.listdir(path)
-    except Exception as ex:
-        print("ERROR:", ex)
-        return
-
-    names = {}
-    for run in sorted(all_runs):
-        run = os.path.join(path, run)
-        creation_date = datetime.fromtimestamp(Path(run).stat().st_mtime)
-        # get the size of this directory (folder)
-        run_size = get_run_size(run, creation_date)
-        print(run, run_size)
-
-        if run_size == 0:
-            continue
-        names[os.path.basename(run)] = run_size
-
-    return names
-
-
 if __name__ == "__main__":
     # proposal = find_proposal("/media/kamile/storage/dssc_data/")
     # print(proposal)
     proposal = 900091
     proposal = find_proposal(proposal, data='raw')
-    get_all_runs_size(proposal)
